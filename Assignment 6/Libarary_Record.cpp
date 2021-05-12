ded7d0e226f72dc1ae90bce424d3fd7ef41b3618
sayalinc2001@gmail.com <sayalinc2001@gmail.com>
2021-05-12 09:03:37 +0530

    Add project files.

---
+++ Assignment 6/Libarary_Record.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class node {
+	string author_name, book_name, publication, isbn_no;
+	node* next;
+public:
+	friend class Library;
+	node(string author, string book, string publication_, string isbn){
+		author_name = author;
+		book_name = book;
+		publication = publication_;
+		isbn_no = isbn;
+		next = NULL;
+	}
+};
+class Library {
+	node* head;
+public:
+	Library() {
+		head = NULL;
+	}
+	void add_new_book();
+	void list_specific_author_book();
+	void search_book();
+	void display();
+
+	//for sorting
+	void sort_books();
+	void swap(node*, node*);
+};
+
+//---------------------ADD NEW BOOK DETAILS TO LIBRARY-------------------
+void Library::add_new_book() {
+	string author, book, publication_, isbn;
+	cout << "Enter book name: ";
+	cin >> book;
+	cout << "Enter author name: ";
+	cin >> author;
+	cout << "Enter publication: ";
+	cin >> publication_;
+	cout << "Enter ISBN number: ";
+	cin >> isbn;
+	node* new_node = new node(author, book, publication_, isbn);
+	if (head == NULL) {
+		head = new_node;
+	}
+	else {
+		node* temp;
+		temp = head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = new_node;
+		new_node->next = NULL;
+	}
+	cout << "\n----------------------------------------------";
+	cout << "\nNew book added to library!";
+	cout << "\nBook name: " << new_node->book_name;
+	cout << "\nAuthor Name: " << new_node->author_name;
+	cout << "\nPublication: " << new_node->publication;
+	cout << "\nISBN Number: " << new_node->isbn_no;
+}
+void Library :: display() {
+	node* temp;
+	cout << "\nISBN NO.\tAUTHOR NAME\t\tBOOK NAME\t\tPUBLICATION";
+	for (temp = head; temp != NULL; temp = temp->next) {
+		cout << "\n" << temp->isbn_no <<"\t\t"<< temp->author_name << "\t\t" << temp->book_name << "\t\t" << temp->publication;
+	}
+}
+
+//------------------LIST ALL BOOKS OF SPECIFIC AUTHOR----------------------
+void Library::list_specific_author_book() {						//Using sequential search
+	node* temp;
+	string specific_author; int flag = 0;
+	cout << "Enter the author name of book you want: ";
+	cin >> specific_author;
+	for (temp = head; temp != NULL; temp = temp->next) {
+		if (specific_author == temp->author_name) {
+			cout <<"\n"<< temp->isbn_no << temp->author_name << "\t\t" << "\t" << temp->book_name << "\t\t" << temp->publication;
+			flag = 1;
+		}
+	}
+	if (flag == 0) {
+		cout << "No such author's book found in the library";
+	}
+}
+
+//-------------------------------SORTING BOOKS-----------------------------
+void Library::swap(node* temp, node* temp2)
+{
+	string n, a, p,i;
+
+	n = temp->book_name;                               //swap temp1=i 
+	a = temp->author_name;
+	p = temp->publication;
+	i = temp->isbn_no;
+
+	// i=j
+	temp->book_name = temp2->book_name;
+	temp->author_name = temp2->author_name;
+	temp->publication = temp2->publication;
+	temp->isbn_no = temp2->isbn_no;
+
+	// j=temp1
+	temp2->book_name = n;
+	temp2->author_name = a;
+	temp2->publication = p;
+	temp2->isbn_no = i;
+}
+void Library::sort_books() {
+		node* i, * j;
+		for (i = head; i->next != NULL; i = i->next)
+		{
+			for (j = i->next; j != NULL; j = j->next)
+			{
+				if (i->isbn_no > j->isbn_no)
+				{
+					swap(i, j);
+				}
+			}
+		}
+		display();
+}
+int main() {
+	Library l;
+	int choice;
+	do {
+		cout << "\t\nWELCOME TO LIBRARY";
+		cout << "\n----------------------------------------------";
+		cout << "\n1. Add new book details \n2. List all books of specific author \n3. Sort books based on ISBN number \n4. Search book in the library";
+		cout << "\n______________________________________________";
+		cout<<"\nEnter your choice : ";
+		cin >> choice;
+		switch (choice) {
+		case 1: l.add_new_book();
+			cout << "\n______________________________________________\n";
+			break;
+		case 2: l.list_specific_author_book();
+			cout << "\n______________________________________________\n";
+			break;
+		case 3:	l.sort_books();
+			break;
+		case 4:	//l.search_book();
+			break;
+		case 5: exit(0);
+		}
+	} while (choice != 5);
+	return 0;
+}
